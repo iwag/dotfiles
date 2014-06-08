7360df82e4463ce3d9089b8107382683316b30be
iwag <iwagame@gmail.com>
2014-06-08 03:52:47 -0700

    add linuxinstall.sh

---
+++ home/.linuxinstall.sh
@@ -0,0 +1,6 @@
+
+PM=apt-get
+# PM = yum
+sudo ${PM} install -y git curl openssh-server zsh gcc g++ autoconf vim ruby rubygems ruby-dev byobu
+# sudo ${PM} install docker
+# sudo ${PM} install npm nodejs 
